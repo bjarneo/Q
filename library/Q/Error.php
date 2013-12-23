bae1d48a88e8bcad288925e0bec6436dde56553e
bjarneo <bjarne@morbid.no>
2013-12-23 23:04:51 +0100

    Error class added. Need more features.

---
+++ library/Q/Error.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Q - a PHP5 Framework
+ *
+ * @author      Bjarne Øverli <http://www.twitter.com/bjarneo_>
+ * @copyright   2013 Bjarne Øverli
+ * @link        https://github.com/bjarneo/Q
+ * @version     0.1.0
+ * @package     Error
+ *
+ * MIT LICENSE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace Q;
+class Error
+{
+    /**
+     * Set error lvl
+     * @var level
+     */
+    protected $level;
+
+    public function __construct() { }
+
+    /**
+     * Set error level
+     * @param $level
+     * @return $this
+     */
+    public function setLevel($level)
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    /**
+     * Return error
+     * Need to add errors to log. Ex logs/system.log or logs/exceptions.log (if exception).
+     */
+    public function getError()
+    {
+        switch(strtolower($this->level)) {
+            case 'development':
+                error_reporting(E_ALL);
+                break;
+            case 'production':
+                error_reporting(0);
+                break;
+            default:
+                error_reporting(E_ERROR | E_WARNING | E_PARSE);
+        }
+    }
+}
+
+?>
---
+++ library/Q/View.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Q - a PHP5 Framework
+ *
+ * @author      Bjarne Øverli <http://www.twitter.com/bjarneo_>
+ * @copyright   2013 Bjarne Øverli
+ * @link        https://github.com/bjarneo/Q
+ * @version     0.1.0
+ * @package     View
+ *
+ * MIT LICENSE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace Q;
+class View
+{
+    /**
+     * Template to render
+     * @var
+     */
+    private $_template;
+
+    /**
+     * Data
+     * @var
+     */
+    private $_data;
+
+
+    /**
+     * Set our template
+     * @param $template
+     * @param $data
+     */
+    public function __construct($template, array $data)
+    {
+        $this->_data = $data;
+
+        try {
+            if(file_exists($template)) {
+                $this->_template = $template;
+            }
+        } catch(Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    /**
+     * Render the view
+     * @return string
+     */
+    public function view()
+    {
+        $html = false;
+
+        if(isset($this->_data)) {
+            extract($this->_data);
+        }
+
+        ob_start();
+        include($this->_template);
+        $html = ob_get_contents();
+        ob_end_clean();
+
+        return $html;
+    }
+}
+
+?>
